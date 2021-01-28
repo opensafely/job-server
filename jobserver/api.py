c66b6ecef2185f8bf89ecd132f19740cb784cbb9
George Hickman <george@ghickman.co.uk>
2021-01-28 14:19:49 +0000

    Send notifications based on JobRequest state

--- jobserver/api.py
+++ jobserver/api.py
@@ -166,8 +166,8 @@ class JobAPIUpdate(APIView):
                 job.save()
                 job.refresh_from_db()
 
-                if not job_request.workspace.should_notify:
-                    # Workspace has notificaitons turned off
+                if not job_request.will_notify:
+                    # Workspace has notifications turned off
                     continue
 
                 if not should_notify:
